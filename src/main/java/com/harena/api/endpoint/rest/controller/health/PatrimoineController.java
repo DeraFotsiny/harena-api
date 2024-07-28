066a1ebc4d3024727968d42ee7be4f9eb58e9ae9
Salohyy212 <128412079+Salohyy212@users.noreply.github.com>
2024-07-28 19:54:57 +0300

    Feat/patrimoine-endpoint (#5)
    * build: add patrimoine as deps
    
    * infra: set own vpc false
    
    * doc: add api specs
    
    * feat(service): implement PatrimoineService for managing patrimoine data
    
    * feat: implement PatrimoineController to handle patrimoine resources
    
    * refactor: object to patrimoine
    
    * fix: change UUID by patrimoine name
    
    * fix: change parameter of crupdatePatrimoine method
    
    * feat: store patrimoine in JSON format and clean up temporary files
    
    * feat: add ComponentScan for school.hei.patrimoine package
    
    * chore: format code
    
    ---------
    
    Co-authored-by: fanomezana <hei.fanomezana.2@gmail.com>

---
+++ src/main/java/com/harena/api/endpoint/rest/controller/health/PatrimoineController.java
@@ -0,0 +1,42 @@
+package com.harena.api.endpoint.rest.controller.health;
+
+import com.harena.api.service.PatrimoineService;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import school.hei.patrimoine.modele.Patrimoine;
+
+@RestController
+public class PatrimoineController {
+  private final PatrimoineService patrimoineService;
+
+  @Autowired
+  public PatrimoineController(PatrimoineService patrimoineService) {
+    this.patrimoineService = patrimoineService;
+  }
+
+  @GetMapping("/patrimoines")
+  public List<Patrimoine> getPatrimoines(
+      @RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "10") int pageSize) {
+    return patrimoineService.getPatrimoines(page, pageSize);
+  }
+
+  @GetMapping("/patrimoines/{nom_patrimoine}")
+  public Patrimoine getPatrimoineByNom(@PathVariable String nom) {
+    return patrimoineService.getPatrimoineByNom(nom);
+  }
+
+  @PutMapping("/patrimoines")
+  public ResponseEntity<Map<String, List<Patrimoine>>> crupdatePatrimoines(
+      @RequestBody Map<String, List<Patrimoine>> newPatrimoines) {
+    newPatrimoines
+        .get("data")
+        .forEach(patrimoine -> patrimoineService.crupdatePatrimoine(patrimoine.nom(), patrimoine));
+    Map<String, List<Patrimoine>> response = new HashMap<>();
+    response.put("data", patrimoineService.getPatrimoines(0, Integer.MAX_VALUE));
+    return ResponseEntity.ok(response);
+  }
+}
