16f3362080dc82be685970e7cbb55041c2dfd26f
nirinaHelena <hei.nirina.7@gmail.com>
2024-07-28 23:24:17 +0300

    feat: ProjectionFutureService::getPatrimoineGraph

--- src/main/java/com/harena/api/service/ProjectionFutureService.java
+++ src/main/java/com/harena/api/service/ProjectionFutureService.java
@@ -1,22 +1,21 @@
 package com.harena.api.service;
 
+import java.io.File;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.Set;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.lang.NonNull;
 import school.hei.patrimoine.modele.EvolutionPatrimoine;
 import school.hei.patrimoine.modele.FluxImpossibles;
 import school.hei.patrimoine.modele.Patrimoine;
-import school.hei.patrimoine.serialisation.Serialiseur;
-
-import java.util.Set;
+import school.hei.patrimoine.visualisation.xchart.GrapheurEvolutionPatrimoine;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "school.hei.patrimoine")
 public class ProjectionFutureService {
   private PatrimoineService patrimoineService;
+  private GrapheurEvolutionPatrimoine grapheurEvolutionPatrimoine;
 
   public Set<FluxImpossibles> getPatrimoineFluxImmpossibles(
       String nomPatrimoine, String debut, String fin) {
@@ -31,5 +30,10 @@ public class ProjectionFutureService {
     LocalDate dateDebut = LocalDate.parse(debut, formatter);
     LocalDate dateFin = LocalDate.parse(fin, formatter);
     return new EvolutionPatrimoine(nomPatrimoine, patrimoine, dateDebut, dateFin);
+  }
+
+  public File getPatrimoineGraph(String nomPatrimoine, String debut, String fin) {
+    EvolutionPatrimoine evolutionPatrimoine = getEvolutionPatrimoine(nomPatrimoine, debut, fin);
+    return grapheurEvolutionPatrimoine.apply(evolutionPatrimoine);
   }
 }
