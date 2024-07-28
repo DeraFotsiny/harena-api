169f2bbbac724061c2799d406bbc835136381878
Salohyy212 <128412079+Salohyy212@users.noreply.github.com>
2024-07-28 21:29:07 +0300

    feat: controller, service for patrimoine
    * build: add patrimoine as deps
    
    * infra: set own vpc false
    
    * doc: add api specs
    
    * feat(service): implement PatrimoineService for managing patrimoine data
    
    * feat: implement PatrimoineController to handle patrimoine resources
    
    * refactor: object to patrimoine
    
    * fix: change UUID by patrimoine name
    
    * fix: change parameter of crupdatePatrimoine method
    
    * feat: store patrimoine in JSON format and clean up temporary files
    
    * feat: add ComponentScan for school.hei.patrimoine package
    
    * chore: format code
    
    ---------
    
    Co-authored-by: fanomezana <hei.fanomezana.2@gmail.com>

--- src/main/java/com/harena/api/PojaApplication.java
+++ src/main/java/com/harena/api/PojaApplication.java
@@ -2,8 +2,10 @@ package com.harena.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "school.hei.patrimoine")
 @PojaGenerated
 public class PojaApplication {
 
