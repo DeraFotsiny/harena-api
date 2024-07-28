54b6834ce54d21baa45072ea2c185d40e68ced2c
Sun-2302 <hei.dera.932@gmail.com>
2024-07-28 20:48:49 +0300

    chore: format code

--- src/main/java/com/harena/api/model/PossessionAvecType.java
+++ src/main/java/com/harena/api/model/PossessionAvecType.java
@@ -12,51 +12,51 @@ import school.hei.patrimoine.modele.possession.Possession;
 @AllArgsConstructor
 @NoArgsConstructor
 public class PossessionAvecType {
-    private String type;
-    private Argent argent;
-    private Materiel materiel;
-    private FluxArgent fluxArgent;
+  private String type;
+  private Argent argent;
+  private Materiel materiel;
+  private FluxArgent fluxArgent;
 
-    public Possession toPossession() {
-        switch (type) {
-            case "ARGENT":
-                return argent;
-            case "MATERIEL":
-                return materiel;
-            case "FLUXARGENT":
-                return fluxArgent;
-            default:
-                throw new IllegalArgumentException("Invalid possession type");
-        }
+  public Possession toPossession() {
+    switch (type) {
+      case "ARGENT":
+        return argent;
+      case "MATERIEL":
+        return materiel;
+      case "FLUXARGENT":
+        return fluxArgent;
+      default:
+        throw new IllegalArgumentException("Invalid possession type");
     }
+  }
 
-    public static PossessionAvecType fromPossession(Possession possession) {
-        PossessionAvecType possessionAvecType = new PossessionAvecType();
-        if (possession instanceof Argent) {
-            possessionAvecType.setType("ARGENT");
-            possessionAvecType.setArgent((Argent) possession);
-        } else if (possession instanceof Materiel) {
-            possessionAvecType.setType("MATERIEL");
-            possessionAvecType.setMateriel((Materiel) possession);
-        } else if (possession instanceof FluxArgent) {
-            possessionAvecType.setType("FLUXARGENT");
-            possessionAvecType.setFluxArgent((FluxArgent) possession);
-        } else {
-            throw new IllegalArgumentException("Invalid possession type");
-        }
-        return possessionAvecType;
+  public static PossessionAvecType fromPossession(Possession possession) {
+    PossessionAvecType possessionAvecType = new PossessionAvecType();
+    if (possession instanceof Argent) {
+      possessionAvecType.setType("ARGENT");
+      possessionAvecType.setArgent((Argent) possession);
+    } else if (possession instanceof Materiel) {
+      possessionAvecType.setType("MATERIEL");
+      possessionAvecType.setMateriel((Materiel) possession);
+    } else if (possession instanceof FluxArgent) {
+      possessionAvecType.setType("FLUXARGENT");
+      possessionAvecType.setFluxArgent((FluxArgent) possession);
+    } else {
+      throw new IllegalArgumentException("Invalid possession type");
     }
+    return possessionAvecType;
+  }
 
-    public String getNom() {
-        switch (type) {
-            case "ARGENT":
-                return argent.getNom();
-            case "MATERIEL":
-                return materiel.getNom();
-            case "FLUXARGENT":
-                return fluxArgent.getNom();
-            default:
-                throw new IllegalArgumentException("Invalid possession type");
-        }
+  public String getNom() {
+    switch (type) {
+      case "ARGENT":
+        return argent.getNom();
+      case "MATERIEL":
+        return materiel.getNom();
+      case "FLUXARGENT":
+        return fluxArgent.getNom();
+      default:
+        throw new IllegalArgumentException("Invalid possession type");
     }
+  }
 }
