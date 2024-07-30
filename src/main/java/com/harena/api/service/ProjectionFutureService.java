6367c7f0d3df8c979aa3fb6278a6226ff135e99b
nirinaHelena <hei.nirina.7@gmail.com>
2024-07-30 09:51:20 +0300

    refactor: find why ProjectionFutureService::getPatrimoineFluxImmpossibles return null

--- src/main/java/com/harena/api/service/ProjectionFutureService.java
+++ src/main/java/com/harena/api/service/ProjectionFutureService.java
@@ -4,31 +4,45 @@ import java.io.File;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.Set;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Service;
 import school.hei.patrimoine.modele.EvolutionPatrimoine;
 import school.hei.patrimoine.modele.FluxImpossibles;
 import school.hei.patrimoine.modele.Patrimoine;
 import school.hei.patrimoine.visualisation.xchart.GrapheurEvolutionPatrimoine;
 
-@SpringBootApplication
-@ComponentScan(basePackages = "school.hei.patrimoine")
+
+@Service
+@AllArgsConstructor
 public class ProjectionFutureService {
+  private static final Logger log = LoggerFactory.getLogger(ProjectionFutureService.class);
   private PatrimoineService patrimoineService;
   private GrapheurEvolutionPatrimoine grapheurEvolutionPatrimoine;
 
-  public Set<FluxImpossibles> getPatrimoineFluxImmpossibles(
+
+    public Set<FluxImpossibles> getPatrimoineFluxImmpossibles(
       String nomPatrimoine, String debut, String fin) {
     EvolutionPatrimoine evolutionPatrimoine = getEvolutionPatrimoine(nomPatrimoine, debut, fin);
+    log.info("\n \nProjectionFutureService::getPatrimoineFluxImpossible(evolutionPatrimoine): {}", evolutionPatrimoine);
     return evolutionPatrimoine.getFluxImpossibles();
   }
 
-  private EvolutionPatrimoine getEvolutionPatrimoine(
+  public EvolutionPatrimoine getEvolutionPatrimoine(
       String nomPatrimoine, String debut, String fin) {
     Patrimoine patrimoine = patrimoineService.getPatrimoineByNom(nomPatrimoine);
+    log.info("ProjectionFutureService::getEvolutionPatrimoine(patrimoine): {}", patrimoine);
+    System.out.println("patrimoine: " +patrimoine);
     DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
     LocalDate dateDebut = LocalDate.parse(debut, formatter);
     LocalDate dateFin = LocalDate.parse(fin, formatter);
+    System.out.println("debut: " +dateDebut+ "\nfin : " + dateFin);
     return new EvolutionPatrimoine(nomPatrimoine, patrimoine, dateDebut, dateFin);
   }
 
